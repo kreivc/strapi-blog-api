2ba0a627d261c7329258ebbe8c61695c49bc5060
Ricky <phangr28@gmail.com>
2021-11-06 01:20:16 +0700

    finish integrate

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.q9nks.mongodb.net '),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi-blog-api'),
-        username: env('DATABASE_USERNAME', 'kreivc'),
-        password: env('DATABASE_PASSWORD', 'Rp534261123'),
+        host: env("DATABASE_HOST", process.env.DATABASE_HOST),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "strapi-blog-api"),
+        username: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
+        password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
